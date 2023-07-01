50bdee57314a753dd4a5fc4ab0be3d8226cfd0ae
rmora18 <ruthmoram18@gmail.com>
2023-07-01 03:05:44 -0300

    entrega final2

--- src/Routes/Favs.jsx
+++ src/Routes/Favs.jsx
@@ -1,30 +1,19 @@
-import React from "react";
+import React from 'react'
+import Card from '../Components/Card'
+import { useGlobalStates } from '../Components/utils/global.context'
 
+const Home = () => {
 
-const Favs = () => {
-
-  const borrarStorage=()=>{
-    localStorage.clear()
-    window.location.reload()
-  }
-
-  const fav=JSON.parse(localStorage.getItem('favs')) 
-
+   const {data}=useGlobalStates()
   return (
-    <>
-      <h1>Dentists favs</h1>
-      <div className="fav" >
-        
-        {fav ? <img src="./images/doctor.jpg" alt=""width={200}height={200} /> : null}
-
-        {fav ? <p>{fav.name} <p>{fav.username}</p></p> : null}
-
-        {fav ? <button className="btn2" onClick={borrarStorage}>Eliminar</button> : null}
-        
+    <main className="" >
+      <h1 >Home</h1>
+      <div className='card-grid'>
+        {data.map((e)=><Card name={e.name} id={e.id} username={e.username}/>)}
+        <Card/>
       </div>
-      
-    </>
+    </main>
   )
 }
 
-export default Favs;
+export default Home
